167af9077cd14e73048ada95c66b13d479320f97
Gabriel <gabrielvt6@gmail.com>
2020-12-18 13:45:49 -0800

    Added an update controller action to handle updating a node's parent_id

--- app/controllers/nodes_controller.rb
+++ app/controllers/nodes_controller.rb
@@ -6,4 +6,26 @@ class NodesController < ApplicationController
       format.json { render json: all_nodes }
     end
   end
+
+  def update
+    node = Node.find(params[:id])
+
+    if node.update(node_params)
+      respond_to do |format|
+        format.html { render html: "Updated node #{node.id}" }
+        format.json { render json: Node.all }
+      end
+    else
+      respond_to do |format|
+        format.html { render html: "Couldn't update node #{node.errors}" }
+        format.json { render json: "Couldn't update node #{node.errors}" }
+      end
+    end
+  end
+
+  private
+
+  def node_params
+    params.require(:node).permit(:parent_id)
+  end
 end
